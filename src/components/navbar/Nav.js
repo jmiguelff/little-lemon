9fd87a9aff6b1ac13157f02e0fb95c66517feb54
Joao Ferreira <jmiguelff@gmail.com>
2023-03-14 19:16:23 +0000

    add fonts, change navbar to navlinks

--- src/components/navbar/Nav.js
+++ src/components/navbar/Nav.js
@@ -1,21 +1,60 @@
 import './Nav.css'
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom";
+
+const Links = [
+  {
+    name: "Home",
+    path: "/",
+    id: "Home"
+  },
+  {
+    name: "About",
+    path: "/about",
+    id: "about"
+  },
+  {
+    name: "Menu",
+    path: "/menu",
+    id: "menu"
+  },
+  {
+    name: "Reservations",
+    path: "/reservations",
+    id: "reservations"
+  },
+  {
+    name: "Order Online",
+    path: "/order",
+    id: "order"
+  },
+  {
+    name: "Login",
+    path: "/login",
+    id: "login"
+  }
+]
 
 function Nav() {
   return (
     <>
       <nav className='nav'>
         <ul className='navbar'>
-          <li><Link to="/" >Home</Link></li>
-          <li><Link to="/about" >About</Link></li>
-          <li><Link to="/menu" >Menu</Link></li>
-          <li><Link to="/reservations" >Reservations</Link></li>
-          <li><Link to="/order" >Order Online</Link></li>
-          <li><Link to="/login" >Login</Link></li>
+          {Links.map((link) => (
+            <li>
+              <NavLink
+                key={link.id}
+                to={link.path}
+                className={({ isActive }) => (isActive ? 'active' : 'inactive')}
+              >
+                {link.name}
+              </NavLink>
+            </li>
+          ))}
         </ul>
       </nav>
     </>
   );
 }
+
 
 export default Nav;
