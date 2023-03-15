fe304577eaf334574486f9c25caf3aff2eb6e193
Joao Ferreira <jmiguelff@gmail.com>
2023-03-15 11:49:01 +0000

    hero section

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,14 +1,28 @@
 import './Hero.css'
+import HeroImage from "../../assets/restauranfood.jpg"
+import { NavLink } from 'react-router-dom';
 
 function Hero() {
   return (
-    <>
-      <main>
-        <div className="hero-container">
-          Hero
+    <div className="hero-background">
+      <div className="hero-section-container">
+        <div className="hero-section-left">
+          <h1 className="hero-title">Little Lemon</h1>
+          <h2 className="hero-subtitle">Chicago</h2>
+          <p className="hero-content">
+            We are a family owned Mediterranean restaurant,
+            focused on traditional recipes served with a modern
+            twist.
+          </p>
+          <NavLink to="/reservations"><button className='btn'>Reserve a Table</button></NavLink>
         </div>
-      </main>
-    </>
+        <div className="hero-section-right">
+          <div className="hero-image">
+            <img src={HeroImage} alt="Delicious food from Little Lemon" />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
