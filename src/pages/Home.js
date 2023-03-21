2cb6365eb3880c8c926911f90701a451b80c26cc
Joao Ferreira <jmiguelff@gmail.com>
2023-03-21 23:00:13 +0000

    add about section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import Header from '../components/header/Header';
 import Hero from '../components/hero/Hero';
 import Main from '../components/main/Main';
 import Testimonials from '../components/testimonials/Testimonials';
+import About from '../components/about/About'
 import Footer from '../components/footer/Footer';
 
 function Home() {
@@ -11,6 +12,7 @@ function Home() {
       <Hero />
       <Main />
       <Testimonials />
+      <About />
       <Footer />
     </>
   );
